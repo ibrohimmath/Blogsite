2448d346df50342564fe1b9f47be3621269d26ee
ibrohimmath <ahmadjonovibrohim062@gmail.com>
2023-08-09 12:14:59 +0500

    blog_site

--- core/core/settings.py
+++ core/core/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-b*9wnefatde(s+j^h23ny_b2+88!xpbu$y6lk4wl6st+@t9npd
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.pythonanywhere.com', '127.0.0.1']
 
 
 # Application definition
